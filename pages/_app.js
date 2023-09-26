a8c4938d8150bb60001908e912f6e178f618caee
maiello97 <matteomaiello97@gmail.com>
2023-09-26 14:14:44 +0200

    fix name e icon

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,22 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import "bootstrap-italia/dist/css/bootstrap-italia.min.css";
 import "typeface-titillium-web";
 import "typeface-roboto-mono";
 import "typeface-lora";
 import { AppContext } from "../context/appContext";
+import Head from "next/head";
 
 const App = ({ Component, pageProps }) => {
   //const pathname = window.location.pathname
-  const [currentPath, setCurrentPath] = useState("")
-  const [codiceVerbale, setCodiceVerbale] = useState(undefined)
-  const [s, setS] = useState(undefined)
+  const [currentPath, setCurrentPath] = useState("");
+  const [codiceVerbale, setCodiceVerbale] = useState(undefined);
+  const [s, setS] = useState(undefined);
 
   useEffect(() => {
-    console.log('subdomain: ', window.location.host.split('.')[0])
-    localStorage.setItem('subdomain', window.location.host.split('.')[0])
-    setS(window.location.host.split('.')[0])
-
-  }, [])
+    console.log("subdomain: ", window.location.host.split(".")[0]);
+    localStorage.setItem("subdomain", window.location.host.split(".")[0]);
+    setS(window.location.host.split(".")[0]);
+  }, []);
 
   /*useEffect(() => {
     setCurrentPath(pathname)
@@ -28,21 +28,28 @@ const App = ({ Component, pageProps }) => {
     const s = localStorage.getItem('subdomain');
     setSubdomain(s)
   }, [subdomain])*/
-  
+
   return (
-    <div>
-      <AppContext.Provider value={{
-        // data,
-        //currentPath,
-        //setCurrentPath,
-        codiceVerbale,
-        setCodiceVerbale,
-        s,
-        setS
-      }}>
-        <Component />
-      </AppContext.Provider>
-    </div>
+    <>
+      <Head>
+        <title>Portale del Cittadino</title>
+      </Head>
+      <div>
+        <AppContext.Provider
+          value={{
+            // data,
+            //currentPath,
+            //setCurrentPath,
+            codiceVerbale,
+            setCodiceVerbale,
+            s,
+            setS,
+          }}
+        >
+          <Component />
+        </AppContext.Provider>
+      </div>
+    </>
   );
 };
 
